c8110fe5e25a82f23c34f85b61347266539bd6d2
SutaroPutraPratama <sutaro635@gmail.com>
2023-01-16 11:19:09 +0700

    tambah

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('assets/css/land.css')}}">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Landing</title>
+</head>
+<body>
+    <div class="title">
+    <h1>WIK-BOOK</h1>
+    </div>
+    <div class="login">
+    <a href="" class="btn btn-dark login">login</a>
+    </div>  
+</body>
+</html>
